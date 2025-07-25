e477b6b12a4495eb542b25ae5d6cfdb5ad68afa5
Hoe Wei Leong <weileonghoe@gmail.com>
2025-07-25 10:05:04 +0800

    refactor: edit code for calling login form at main method

--- src/apumedicalcentre/ApuMedicalCentre.java
+++ src/apumedicalcentre/ApuMedicalCentre.java
@@ -6,11 +6,9 @@ import login.loginForm;
 public class ApuMedicalCentre extends JFrame {
 
     public static void main(String[] args) {
-        // always start Swing apps on the Event‑Dispatch Thread
-        SwingUtilities.invokeLater(loginForm::new);
-//        SwingUtilities.invokeLater(() -> {
-//            new loginForm().setVisible(true);
-//        });
+        SwingUtilities.invokeLater(() -> {
+            new loginForm().setVisible(true);
+        });
     }
 }
 
