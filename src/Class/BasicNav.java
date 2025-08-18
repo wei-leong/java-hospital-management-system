481a3da3a181734683febf4a370287c438812148
Hoe Wei Leong <weileonghoe@gmail.com>
2025-08-18 12:54:22 +0800

    feat: add image to constructor

--- src/Class/BasicNav.java
+++ src/Class/BasicNav.java
@@ -4,11 +4,19 @@
  */
 package Class;
 
+import java.awt.BorderLayout;
 import java.awt.CardLayout;
+import java.awt.Image;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import static javax.swing.WindowConstants.DISPOSE_ON_CLOSE;
 
 /**
  *
@@ -25,10 +33,17 @@ public class BasicNav extends JFrame{
     private String currentPage = "";
     private final JLabel lblTitle = new JLabel();
     
+    // Image Setup
+    private final ImageIcon toggleIcon = imgScale.returnScaledImageIcon("/image/nav-menu.png", 24, 24);
+    Image windowIcon = imgScale.returnScaledImage("/image/APU_Med_Cen_Assignment.png", 128, 128);
+    Icon iconProfile = imgScale.returnScaledImageIcon("/image/profile-user.png", 32, 32);
+    Icon iconProfileLarge = imgScale.returnScaledImageIcon("/image/profile-user.png", 50, 50);
+    
     // Button Attributes
-    private final JButton btnToggle = new JButton();
+    private final JButton btnToggle = new JButton(toggleIcon);
     private final JButton btnLogout= new JButton("Logout");
     private final JButton btnEdit = new JButton("Edit Profile");
     private final JButton btnProfile = new JButton();
+    
     
 }
