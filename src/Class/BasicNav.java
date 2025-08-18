ebac30f97dd11f804d8d57dc862a0b03723df682
Hoe Wei Leong <weileonghoe@gmail.com>
2025-08-18 12:44:49 +0800

    feat: create BasicNav class to store Navigation UI to provide reusability for other role

---
+++ src/Class/BasicNav.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Class;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Wlhoe
+ */
+public class BasicNav extends JFrame{
+    
+}
