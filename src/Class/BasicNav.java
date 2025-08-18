180837328293e0d140b94f7498da40b4cd4d4cf5
Hoe Wei Leong <weileonghoe@gmail.com>
2025-08-18 12:49:24 +0800

    feat: add attributes to BasicNav class

--- src/Class/BasicNav.java
+++ src/Class/BasicNav.java
@@ -4,12 +4,31 @@
  */
 package Class;
 
+import java.awt.CardLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 /**
  *
  * @author Wlhoe
  */
 public class BasicNav extends JFrame{
+    // Own Staff Details - Remove own profile from StaffManagement page
+    private final String[] _staffDetails;
+    private final ImageScaler imgScale = new ImageScaler();
+    
+    private final JPanel sidebar;
+    private final CardLayout cards = new CardLayout();
+    private final JPanel content= JPanel(cards);
+    private String currentPage = "";
+    private final JLabel lblTitle = new JLabel();
+    
+    // Button Attributes
+    private final JButton btnToggle = new JButton();
+    private final JButton btnLogout= new JButton("Logout");
+    private final JButton btnEdit = new JButton("Edit Profile");
+    private final JButton btnProfile = new JButton();
     
 }
