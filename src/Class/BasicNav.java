c148d838f31a1965171865898169b0f72ef3674f
Hoe Wei Leong <weileonghoe@gmail.com>
2025-08-18 13:04:45 +0800

    feat: create method buildToolBar with other methods

--- src/Class/BasicNav.java
+++ src/Class/BasicNav.java
@@ -99,4 +99,100 @@ public class BasicNav extends JFrame{
 
         return titleBar;
     }
+    
+        
+    protected void showProfileDialog() {
+        // Create dialog to show Staff Details
+        JDialog dlg = new JDialog(this, "Profile", true);
+        dlg.setLayout(new BorderLayout(10, 10));
+        dlg.setResizable(false);
+
+        // Header (Profile Picture + Staff Id and Staff Role)
+        JLabel picLabel = new JLabel(iconProfileLarge);
+        picLabel.setBorder(BorderFactory.createEmptyBorder(0, 10, 0, 0));
+
+        // Create JPanel to group profile picture, staff id and staff role together
+        JPanel idRole = new JPanel(new GridLayout(0, 1, 5, 5));
+        idRole.add(new JLabel(_staffDetails[0])); // Staff ID
+        idRole.add(new JLabel(_staffDetails[1])); // Staff Role
+
+        JPanel header = new JPanel(new BorderLayout(10, 10));
+        header.add(picLabel, BorderLayout.WEST);
+        header.add(idRole, BorderLayout.CENTER);
+        dlg.add(header, BorderLayout.NORTH);
+
+        // Details (in the CENTER so it expands naturally)
+        JPanel details = new JPanel(new GridLayout(0, 1, 5, 5));
+        details.setBorder(BorderFactory.createEmptyBorder(0, 20, 0, 20));
+        details.add(new JLabel(_staffDetails[2])); // Staff Name
+        details.add(new JLabel(_staffDetails[5])); // Staff Email
+        details.add(new JLabel(_staffDetails[6])); // Staff Phone Number
+        details.add(new JLabel(_staffDetails[7])); // Staff Age
+        dlg.add(details, BorderLayout.CENTER);
+
+        // Edit Profile Button 
+        JPanel bottom = new JPanel(new BorderLayout(10, 10));
+
+        bottom.add(btnEdit, BorderLayout.CENTER);
+        bottom.setBorder(BorderFactory.createEmptyBorder(0, 20, 20, 20));
+        dlg.add(bottom, BorderLayout.SOUTH);
+        btnEditSettings();
+
+        // Finalize size & position
+        dlg.setSize(300, 250);  // fixed size you prefer
+        Point loc = this.getLocationOnScreen();
+        dlg.setLocation(loc.x + this.getWidth() - dlg.getWidth() - 10, loc.y + 10);
+        dlg.setVisible(true);
+    }
+    
+    protected void btnToggleSettings() {
+        btnToggle.setFocusable(false);
+        btnToggle.setBorderPainted(false);
+        btnToggle.setContentAreaFilled(false);
+        btnToggle.setFocusPainted(false);
+        btnToggle.setOpaque(false);
+        btnToggle.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        // Toggle Sidebar Visibility
+        btnToggle.addActionListener(e
+                -> sidebar.setVisible(!sidebar.isVisible())
+        );
+    }
+
+    protected void btnLogoutSettings(JPanel bar) {
+        btnLogout.setBackground(Color.WHITE);
+        btnLogout.setForeground(Color.BLACK);
+        btnLogout.setPreferredSize(new Dimension(150, 35));
+        btnLogout.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        btnLogout.setOpaque(true);
+        btnLogout.setContentAreaFilled(true);
+        btnLogout.setFocusPainted(false);
+        btnLogout.addActionListener(e -> {
+            // Dispose NavManager JFrame
+            SwingUtilities.getWindowAncestor(bar).dispose();
+            // Open Login Form
+            login.LoginForm login = new login.LoginForm();
+            login.setVisible(true);
+        });
+    }
+
+    protected void btnEditSettings() {
+        btnEdit.setBackground(Color.white);
+        btnEdit.setForeground(Color.BLACK);
+        btnEdit.setPreferredSize(new Dimension(150, 35));
+        btnEdit.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        btnEdit.setOpaque(true);
+        btnEdit.setContentAreaFilled(true);
+        btnEdit.setFocusPainted(false);
+        btnEdit.addActionListener(e -> {
+            // Edit Own Profile Logic Here
+        });
+    }
+
+    protected void btnProfileSettings() {
+        btnProfile.setBorder(null);
+        btnProfile.setContentAreaFilled(false);
+
+        // Show Own Profile Details and Enable user to edit their own profile
+        btnProfile.addActionListener(e -> showProfileDialog());
+    }
 }
